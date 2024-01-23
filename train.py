5cc48e28b64a8f82995da121a520496b378817f0
刘世旭 <liushixu@bytedance.com>
2024-01-24 01:42:31 +0800

    feat(main): add program entry file

--- train.py
+++ train.py
@@ -1,5 +1,26 @@
-import tensorflow as tf
+import argparse
+
+import palmnet
 
 if __name__ == "__main__":
-    print(tf.config.list_physical_devices("GPU"))
-    print(tf.config.list_physical_devices("CPU"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset",
+        dest="datasets",
+        metavar="d",
+        type=palmnet.SupportedDatasetName,
+        default=[],
+        nargs="+",
+        help="train which datasets, default is all",
+    )
+    parser.add_argument("--epochs", type=int, default=64, help="epochs to train")
+    parser.add_argument("--batch-size", type=int, default=32, help="batch size")
+    parser.add_argument("--download", action="store_true", default=False, help="download dataset")
+    args = parser.parse_args()
+
+    if args.download:
+        datasets = args.datasets or list(palmnet.SupportedDatasetName)
+        for dataset in datasets:
+            palmnet.download_dataset(dataset)
+    else:
+        palmnet.train(dataset_names=args.datasets, epochs=args.epochs, batch_size=args.batch_size)
